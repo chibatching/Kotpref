4d2bca174f63e27aa71bd48a716f3e114ff5e271
Takao Chiba <chiba-takao@cyber-z.co.jp>
2015-08-05 13:35:53 +0900

    Move delegation method to KotprefModel

--- kotpref/src/main/kotlin/com/chibatching/kotpref/Kotpref.kt
+++ kotpref/src/main/kotlin/com/chibatching/kotpref/Kotpref.kt
@@ -1,16 +1,12 @@
 package com.chibatching.kotpref
 
 import android.content.Context
-import android.content.SharedPreferences
-import android.preference.PreferenceManager
-import kotlin.properties.Delegates
-import kotlin.properties.ReadWriteProperty
 
 public object Kotpref {
 
     // context
     var context: Context? = null
-        get() { if ($context != null) { return $context } else { throw IllegalStateException("Context has not yet set") }}
+        get() = if ($context != null) { $context } else { throw IllegalStateException("Context has not yet set") }
         private set
 
     /**
@@ -20,23 +16,3 @@ public object Kotpref {
         this.context = context.getApplicationContext()
     }
 }
-
-
-public fun KotprefModel.stringPrefVar(default: String = "")
-        : ReadWriteProperty<KotprefModel, String> = StringPrefVar(default)
-
-public fun KotprefModel.intPrefVar(default: Int = 0)
-        : ReadWriteProperty<KotprefModel, Int> = IntPrefVar(default)
-
-public fun KotprefModel.longPrefVar(default: Long = 0L)
-        : ReadWriteProperty<KotprefModel, Long> = LongPrefVar(default)
-
-public fun KotprefModel.floatPrefVar(default: Float = 0F)
-        : ReadWriteProperty<KotprefModel, Float> = FloatPrefVar(default)
-
-public fun KotprefModel.booleanPrefVar(default: Boolean = false)
-        : ReadWriteProperty<KotprefModel, Boolean> = BooleanPrefVar(default)
-
-public fun KotprefModel.stringSetPrefVar(default: Set<String>)
-        : ReadWriteProperty<KotprefModel, MutableSet<String>> = StringSetPrefVar(default)
-
--- kotpref/src/main/kotlin/com/chibatching/kotpref/KotprefModel.kt
+++ kotpref/src/main/kotlin/com/chibatching/kotpref/KotprefModel.kt
@@ -2,8 +2,8 @@ package com.chibatching.kotpref
 
 import android.content.Context
 import android.content.SharedPreferences
-import android.preference.PreferenceManager
 import kotlin.properties.Delegates
+import kotlin.properties.ReadWriteProperty
 
 
 public open class KotprefModel() {
@@ -18,4 +18,22 @@ public open class KotprefModel() {
     public fun clear() {
         kotprefPreference.edit().clear().apply()
     }
+
+    protected fun stringPrefVar(default: String = "")
+            : ReadWriteProperty<KotprefModel, String> = StringPrefVar(default)
+
+    protected fun intPrefVar(default: Int = 0)
+            : ReadWriteProperty<KotprefModel, Int> = IntPrefVar(default)
+
+    protected fun longPrefVar(default: Long = 0L)
+            : ReadWriteProperty<KotprefModel, Long> = LongPrefVar(default)
+
+    protected fun floatPrefVar(default: Float = 0F)
+            : ReadWriteProperty<KotprefModel, Float> = FloatPrefVar(default)
+
+    protected fun booleanPrefVar(default: Boolean = false)
+            : ReadWriteProperty<KotprefModel, Boolean> = BooleanPrefVar(default)
+
+    protected fun stringSetPrefVar(default: Set<String>)
+            : ReadWriteProperty<KotprefModel, MutableSet<String>> = StringSetPrefVar(default)
 }
