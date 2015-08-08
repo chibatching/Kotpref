16c5a676b73bba49eeaf47332c4f60b16b392865
Takao Chiba <takao.chiba@gmail.com>
2015-08-08 16:12:31 +0900

    Change float sample property name.

--- sample/src/main/kotlin/com/chibatching/kotprefsample/UserInfo.kt
+++ sample/src/main/kotlin/com/chibatching/kotprefsample/UserInfo.kt
@@ -8,7 +8,7 @@ public object UserInfo : KotprefModel() {
     var name: String by stringPrefVar()
     var age: Int by intPrefVar()
     var highScore: Long by longPrefVar()
-    var threshold: Float by floatPrefVar()
+    var rate: Float by floatPrefVar()
     val prizes: MutableSet<String> by stringSetPrefVal {
         val set = TreeSet<String>()
         set.add("Beginner")
