143107183dc914e2a549b748b5b597ef364a659b
Takao Chiba <takao.chiba@gmail.com>
2016-09-01 00:06:25 +0900

    Add content provider to init kotpref

---
+++ kotpref/src/main/kotlin/com/chibatching/kotpref/KotprefInitProvider.kt
@@ -0,0 +1,34 @@
+package com.chibatching.kotpref
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+
+
+class KotprefInitProvider : ContentProvider() {
+    override fun insert(uri: Uri?, values: ContentValues?): Uri? {
+        return null
+    }
+
+    override fun query(uri: Uri?, projection: Array<out String>?, selection: String?, selectionArgs: Array<out String>?, sortOrder: String?): Cursor? {
+        return null
+    }
+
+    override fun onCreate(): Boolean {
+        Kotpref.init(context)
+        return true
+    }
+
+    override fun update(uri: Uri?, values: ContentValues?, selection: String?, selectionArgs: Array<out String>?): Int {
+        return 0
+    }
+
+    override fun delete(uri: Uri?, selection: String?, selectionArgs: Array<out String>?): Int {
+        return 0
+    }
+
+    override fun getType(uri: Uri?): String? {
+        return null
+    }
+}
