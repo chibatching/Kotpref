8bcfa9eb418fccb89a7dab4278f29e47e05328a1
Takao Chiba <takao.chiba@gmail.com>
2016-09-01 01:04:14 +0900

    Replace RobolectricGradleTestRunner with RobolectricTestRunner

--- kotpref/src/test/java/com/chibatching/kotpref/KotprefBasicTest.kt
+++ kotpref/src/test/java/com/chibatching/kotpref/KotprefBasicTest.kt
@@ -8,13 +8,13 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.RobolectricGradleTestRunner
+import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
 import org.robolectric.annotation.Config
 import java.util.*
 
 
-@RunWith(RobolectricGradleTestRunner::class)
+@RunWith(RobolectricTestRunner::class)
 @Config(constants = BuildConfig::class, manifest = Config.NONE, sdk = intArrayOf(23))
 class KotprefBasicTest {
 
--- kotpref/src/test/java/com/chibatching/kotpref/KotprefCustomTest.kt
+++ kotpref/src/test/java/com/chibatching/kotpref/KotprefCustomTest.kt
@@ -7,11 +7,11 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.RobolectricGradleTestRunner
+import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
 import org.robolectric.annotation.Config
 
-@RunWith(RobolectricGradleTestRunner::class)
+@RunWith(RobolectricTestRunner::class)
 @Config(constants = BuildConfig::class, manifest = Config.NONE, sdk = intArrayOf(23))
 class KotprefCustomTest {
 
