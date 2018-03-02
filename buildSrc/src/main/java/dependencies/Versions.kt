93207a67f66071593651295113fea3df87958080
Takao Chiba <takao.chiba@gmail.com>
2018-03-02 16:16:33 +0900

    Manage dependencies version using buildSrc+Kotlin

---
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -0,0 +1,25 @@
+package dependencies
+
+object Versions {
+
+    const val versionName = "2.4.0"
+
+    const val targetSdk = 27
+    const val minSdk = 9
+    const val minSdkWithSupportLibrary = 14
+    const val buildTools = "27.0.2"
+
+    internal const val kotlin = "1.2.21"
+    internal const val junit = "4.12"
+    internal const val robolectric = "3.4.2"
+    internal const val assertj = "3.8.0"
+    internal const val testing = "0.1"
+    internal const val gson = "2.8.2"
+    internal const val arch = "1.1.0"
+    internal const val supportLibrary = "27.0.2"
+
+    internal const val androidGradlePlugin = "3.0.1"
+    internal const val bintrayGradlePlugin = "1.7.3"
+    internal const val androidMavenGradlePlugin = "2.0"
+    internal const val dokkaAndroidGradlePlugin = "0.9.15"
+}
