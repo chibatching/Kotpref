d42db3dbd33b612f096343b251dd1e48ed475c6a
Takao Chiba <takao.chiba@gmail.com>
2018-03-27 16:21:08 +0900

    Extract jacoco settings to helper plugin

---
+++ buildSrc/src/main/java/plugin/BuildSettingHelperPlugin.kt
@@ -0,0 +1,52 @@
+package plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.closureOf
+import org.gradle.testing.jacoco.plugins.JacocoPluginExtension
+import org.gradle.testing.jacoco.tasks.JacocoReport
+
+class BuildSettingHelperPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) {
+        project.extensions.add("isCi", System.getenv("CI")?.toBoolean() ?: false)
+        addJacocoSettings(project)
+    }
+
+    private fun addJacocoSettings(project: Project) {
+        project.closureOf<JacocoPluginExtension> {
+            toolVersion = "0.8.0"
+        }
+        project.tasks.create("jacocoTestReport", JacocoReport::class.java) {
+            dependsOn("testDebugUnitTest")
+            reports {
+                xml.isEnabled = true
+                html.isEnabled = true
+            }
+            val fileFilter = listOf(
+                "**/R.class", "**/R$*.class", "**/BuildConfig.*", "**/Manifest*.*"
+            )
+            val debugTree = project.fileTree(
+                mapOf(
+                    "dir" to "${project.buildDir}/intermediates/classes/debug",
+                    "excludes" to fileFilter
+                )
+            )
+            val kotlinDebugTree = project.fileTree(
+                mapOf(
+                    "dir" to "${project.buildDir}/tmp/kotlin-classes/debug",
+                    "excludes" to fileFilter
+                )
+            )
+            val mainSrc = "${project.projectDir}/src/main/kotlin"
+            sourceDirectories = project.files(listOf(mainSrc))
+            classDirectories = project.files(listOf(debugTree, kotlinDebugTree))
+            executionData = project.fileTree(
+                mapOf(
+                    "dir" to project.buildDir.toString(),
+                    "includes" to listOf("jacoco/testDebugUnitTest.exec")
+                )
+            )
+        }
+    }
+}
