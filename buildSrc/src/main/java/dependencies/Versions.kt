92a7bd21d8f06fae3122d63704a4bf244a9e3429
Takao Chiba <takao.chiba@gmail.com>
2018-10-12 16:10:57 +0900

    Update dependencies

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -4,25 +4,25 @@ object Versions {
 
     const val versionName = "2.6.0"
 
-    const val targetSdk = 27
+    const val targetSdk = 28
     const val minSdk = 9
     const val minSdkWithSupportLibrary = 14
-    const val buildTools = "28.0.1"
+    const val buildTools = "28.0.3"
 
-    internal const val kotlin = "1.2.60"
+    internal const val kotlin = "1.2.71"
     internal const val junit = "4.12"
     internal const val robolectric = "3.8"
-    internal const val assertj = "3.9.1"
+    internal const val assertj = "3.11.1"
     internal const val gson = "2.8.5"
     internal const val arch = "1.1.1"
     internal const val supportLibrary = "27.1.1"
 
-    internal const val androidGradlePlugin = "3.2.0-beta05"
+    internal const val androidGradlePlugin = "3.2.1"
     internal const val bintrayGradlePlugin = "1.8.4"
     internal const val androidMavenGradlePlugin = "2.1"
     internal const val dokkaAndroidGradlePlugin = "0.9.17"
 
-    internal const val jacoco = "0.8.1"
-    internal const val mockito = "2.21.0"
+    internal const val jacoco = "0.8.2"
+    internal const val mockito = "2.23.0"
     internal const val mockitoKotlin = "1.6.0"
 }
