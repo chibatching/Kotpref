c1e9eba1f078f7bd7437dedc39a03cd807aa443a
Takao Chiba <takao.chiba@gmail.com>
2020-04-29 22:49:12 +0900

    Update dependencies

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -8,16 +8,15 @@ object Versions {
     const val minSdk = 9
     const val minSdkWithSupportLibrary = 14
 
-    internal const val kotlin = "1.3.61"
+    internal const val kotlin = "1.3.72"
     internal const val junit = "4.12"
     internal const val robolectric = "4.3"
-    internal const val gson = "2.8.5"
-    internal const val liveData = "2.1.0"
+    internal const val gson = "2.8.6"
 
-    internal const val androidGradlePlugin = "3.5.3"
-    internal const val bintrayGradlePlugin = "1.8.4"
+    internal const val androidGradlePlugin = "3.6.3"
+    internal const val bintrayGradlePlugin = "1.8.5"
     internal const val androidMavenGradlePlugin = "2.1"
-    internal const val dokkaGradlePlugin = "0.10.0"
+    internal const val dokkaGradlePlugin = "0.10.1"
 
     internal const val jacoco = "0.8.5"
     internal const val mockk = "1.9.3"
@@ -26,8 +25,9 @@ object Versions {
     internal object AndroidX {
         internal const val annotation = "1.1.0"
         internal const val appCompat = "1.1.0"
-        internal const val preference = "1.1.0"
-        internal const val security = "1.0.0-alpha02"
+        internal const val preference = "1.1.1"
+        internal const val security = "1.0.0-rc01"
+        internal const val liveData = "2.2.0"
         internal const val test = "1.2.0"
     }
 }
