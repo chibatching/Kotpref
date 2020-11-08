5a1cba9c2f9df1b84f826fb3bef1498a8fd4c2f0
Takao Chiba <takao.chiba@gmail.com>
2020-11-08 19:01:56 +0900

    Upgrade AGP

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -13,7 +13,7 @@ object Versions {
     internal const val robolectric = "4.4"
     internal const val gson = "2.8.6"
 
-    internal const val androidGradlePlugin = "4.0.1"
+    internal const val androidGradlePlugin = "4.1.0"
     internal const val bintrayGradlePlugin = "1.8.5"
     internal const val dokka = "1.4.10"
 
