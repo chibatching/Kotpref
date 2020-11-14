1d1755a3a0f05e775c4a78d4b56c4311e788628f
Takao Chiba <takao.chiba@gmail.com>
2020-11-14 14:53:12 +0900

    Update api doc

--- docs/api/livedata-support/scripts/sourceset_dependencies.js
+++ docs/api/livedata-support/scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies = '{":livedata-support:dokkaHtml/debug": [], ":livedata-support:dokkaHtml/main": [], ":livedata-support:dokkaHtml/release": []}'
+sourceset_dependencies = '{":livedata-support:dokkaHtml/androidTestRelease": [], ":livedata-support:dokkaHtml/debug": [], ":livedata-support:dokkaHtml/main": [], ":livedata-support:dokkaHtml/release": []}'
