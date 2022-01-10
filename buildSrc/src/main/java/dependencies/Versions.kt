7e831c8486e6d89dd4856d8e1063f2adfe55531e
Takao Chiba <takao.chiba@gmail.com>
2022-01-10 23:38:58 +0900

    Update third party libs

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -11,7 +11,7 @@ object Versions {
     internal const val kotlin = "1.6.10"
     internal const val junit = "4.13.2"
     internal const val robolectric = "4.7.3"
-    internal const val gson = "2.8.6"
+    internal const val gson = "2.8.9"
 
     internal const val androidGradlePlugin = "7.0.4"
     internal const val dokka = "1.6.10"
@@ -21,12 +21,12 @@ object Versions {
     internal const val truth = "1.1.3"
 
     internal object AndroidX {
-        internal const val annotation = "1.1.0"
-        internal const val appCompat = "1.2.0"
+        internal const val annotation = "1.3.0"
+        internal const val appCompat = "1.4.0"
         internal const val preference = "1.1.1"
-        internal const val security = "1.0.0-rc03"
-        internal const val startup = "1.0.0"
-        internal const val liveData = "2.2.0"
-        internal const val test = "1.3.0"
+        internal const val security = "1.0.0"
+        internal const val startup = "1.1.0"
+        internal const val liveData = "2.4.0"
+        internal const val test = "1.4.0"
     }
 }
