de364cc75bb4038528fbe6901fdd684399518383
Takao Chiba <takao.chiba@gmail.com>
2022-01-10 23:14:43 +0900

    Update kotlin and test libraries

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -8,17 +8,17 @@ object Versions {
     const val minSdk = 9
     const val minSdkWithSupportLibrary = 14
 
-    internal const val kotlin = "1.4.10"
-    internal const val junit = "4.13.1"
-    internal const val robolectric = "4.4"
+    internal const val kotlin = "1.6.10"
+    internal const val junit = "4.13.2"
+    internal const val robolectric = "4.7.3"
     internal const val gson = "2.8.6"
 
     internal const val androidGradlePlugin = "7.0.4"
     internal const val dokka = "1.4.10.2"
 
-    internal const val jacoco = "0.8.6"
-    internal const val mockk = "1.10.2"
-    internal const val truth = "1.1"
+    internal const val jacoco = "0.8.7"
+    internal const val mockk = "1.12.2"
+    internal const val truth = "1.1.3"
 
     internal object AndroidX {
         internal const val annotation = "1.1.0"
